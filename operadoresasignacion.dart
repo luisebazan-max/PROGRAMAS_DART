e05efd8f40e01ac40aae7da50c6cadd1388956f1
luisebazan-max <luise.bazan@cbtis265.edu.mx>
2026-03-04 12:51:06 -0600

    correcion

--- operadoresasignacion.dart
+++ operadoresasignacion.dart
@@ -1,6 +1,6 @@
 void main(){
   double edad = 24;
-  double e = 24;
+  
   edad += 1;
   print( "despues de usar el operador +=1 la edad es $edad");
 
@@ -9,11 +9,11 @@ void main(){
   
 
   edad *= 1;
-  print( "despues de usar el operador +=1 la edad es $edad");
+  print( "despues de usar el operador *=1 la edad es $edad");
   
 
   edad /= 1;
-  print( "despues de usar el operador +=1 la edad es $edad");
+  print( "despues de usar el operador /=1 la edad es $edad");
   
 
 
